22e46db91066a92ecc63811c2e723bff5aa2d1e3
JavaTota <jomigone@gmail.com>
2025-03-21 10:07:20 -0500

    Add todo functionality with InputField component; implement state management for todos

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./style.css";
 
-const InputField = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }: Props) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <form className="input">
-      <input type="input" placeholder="Write a task" className="input__box" />
+    <form
+      className="input"
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
+      <input
+        ref={inputRef}
+        value={todo}
+        onChange={(e) => {
+          setTodo(e.target.value);
+        }}
+        type="input"
+        placeholder="Write a task"
+        className="input__box"
+      />
       <button className="input__submit" type="submit">
         Go
       </button>
